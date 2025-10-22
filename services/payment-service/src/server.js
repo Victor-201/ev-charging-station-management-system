1cb886c56cf48fc401550d19b6cb42dc34b6a339
Victor-201 <4.victor.201@gmail.com>
2025-10-22 19:46:42 +0700

    build(docker): successfully packaged payment-service, postgres, and rabbitmq containers

--- services/payment-service/src/server.js
+++ services/payment-service/src/server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 import env from './config/env.js';
 
-const port = env.PORT || 3000;
+const port = env.PORT || 8080;
 app.listen(port, () => {
   console.log(`payment-service listening on port ${port}`);
 });
