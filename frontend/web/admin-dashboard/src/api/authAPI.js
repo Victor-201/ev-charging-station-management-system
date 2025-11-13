515f442b1a9d37f91544035a9458f1c931e7aee3
Victor-201 <4.victor.201@gmail.com>
2025-11-13 14:26:24 +0700

    Remove integrated web-admin and web-user from ev-charging-portal

--- frontend/web/admin-dashboard/src/api/authAPI.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "./axiosClient";
-
-const authAPI = {
-  login: (data) => axiosClient.post("/auth/login", data),
-  logout: () => localStorage.removeItem("access_token"),
-};
-
-export default authAPI;
