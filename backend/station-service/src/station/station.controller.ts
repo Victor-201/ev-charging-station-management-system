2fef0513bd115d695a084c316607e1e9adac2fea
bedive <trungbed1ve@gmail.com>
2025-11-20 16:23:43 +0700

    fix: change path of get all report issues from report-issues to report-issues/all

--- backend/station-service/src/station/station.controller.ts
+++ backend/station-service/src/station/station.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Post, Body, Put, Delete, UseGuards, Param, Req } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body, Put, Delete, UseGuards, Param, Req, HttpException, HttpStatus } from '@nestjs/common';
 import { StationService } from './station.service';
 
 import type { Request } from 'express';
@@ -112,8 +112,22 @@ export class StationController {
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('admin', 'staff', 'user')
     @Post('ability')
-    async getStationAbility( @Body() body: StationAbilityItemDto): Promise<StationAbilityDto> {
-        return this.stationService.getStationAbility(body);
+    async getStationAbility(@Body() body: StationAbilityItemDto): Promise<StationAbilityDto> {
+        try {
+            return await this.stationService.getStationAbility(body);
+        } catch (error) {
+            throw new HttpException(
+                { message: error.message || 'Timeout waiting for response' },
+                HttpStatus.REQUEST_TIMEOUT
+            );
+        }
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('admin', 'staff', 'user')
+    @Get('report-issues/all')
+    async getAllHistoryOfReports() {
+        return this.stationService.getAllHistoryOfReports();
     }
 
     @UseGuards(JwtAuthGuard, RolesGuard)
@@ -123,10 +137,4 @@ export class StationController {
         return this.stationService.getHistoryOfReports(id);
     }
 
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles('admin', 'staff')
-    @Get('/report-issues')
-    async getAllHistoryOfReports () {
-        return this.stationService.getAllHistoryOfReports();
-    }
 }
