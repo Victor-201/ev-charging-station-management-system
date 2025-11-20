175ee6baf9a19c869b5edbe81d93cb0c99262303
Victor-201 <4.victor.201@gmail.com>
2025-11-21 01:21:26 +0700

    refactor(payment): update env and server configuration

--- backend/payment-service/src/server.js
+++ backend/payment-service/src/server.js
@@ -1,7 +1,20 @@
 import app from './app.js';
-import env from './config/env.js';
+import config from './config/env.js';
+import { initRabbitConnection } from './core/rabbit/connection.js';
 
-const port = env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`payment-service listening on port ${port}`);
-});
+const port = config.PORT;
+
+(async () => {
+  try {
+    console.log("Initializing RabbitMQ connection for Payment Service...");
+    await initRabbitConnection();
+
+    app.listen(port, () => {
+      console.log(`payment-service listening on port ${port}`);
+    });
+
+  } catch (err) {
+    console.error("Failed to start payment-service:", err.message);
+    process.exit(1);
+  }
+})();
