1799f507ba442b01140e34f25cecef3c6ca09057
Victor-201 <4.victor.201@gmail.com>
2025-11-22 02:28:42 +0700

    refactor(payment): update invoice routes

--- backend/payment-service/src/routes/invoiceRoutes.js
+++ backend/payment-service/src/routes/invoiceRoutes.js
@@ -12,6 +12,6 @@ router.patch('/:invoice_id/pay', authorize(UserRole.ADMIN), InvoiceController.ma
 
 router.get('/overdue', authorize(UserRole.ADMIN), InvoiceController.listOverdueInvoices);
 router.get('/user/:user_id', authorize(UserRole.ADMIN, UserRole.USER), InvoiceController.listInvoicesByUser);
-router.get('/:invoice_id', authorize(UserRole.ADMIN, UserRole.USER), InvoiceController.getInvoiceById);
+router.get('/:invoice_id',  InvoiceController.getInvoiceById);
 
 export default router;
