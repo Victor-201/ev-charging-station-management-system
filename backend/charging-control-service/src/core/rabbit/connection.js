d645b7936c4926e4c6e947e3ba8eb01f39677730
Victor-201 <4.victor.201@gmail.com>
2025-11-22 17:56:43 +0700

    refactor(charging-control-service): update rabbit connection & consumer

--- backend/charging-control-service/src/core/rabbit/connection.js
+++ backend/charging-control-service/src/core/rabbit/connection.js
@@ -1,22 +1,22 @@
+// core/rabbit/connection.js
 const amqp = require("amqplib");
 const config = require("../../config/env.js");
 
 let connection = null;
 let channel = null;
 
-const RMQ_EXCHANGE = config.EXCHANGE_NAME;
+const RMQ_EXCHANGE = config.EXCHANGE_NAME || "payments";
 
 async function initRabbitConnection() {
   if (connection) return { connection, channel };
 
-  const RABBIT_URL = config.RABBITMQ_URL;
-
-  connection = await amqp.connect(RABBIT_URL);
+  connection = await amqp.connect(config.RABBITMQ_URL);
   channel = await connection.createChannel();
 
+  // Topic exchange cho microservice
   await channel.assertExchange(RMQ_EXCHANGE, "direct", { durable: true });
 
-  console.log("[RABBIT] Charging connected");
+  console.log(`[RABBIT] Connected to exchange: ${RMQ_EXCHANGE}`);
 
   return { connection, channel };
 }
