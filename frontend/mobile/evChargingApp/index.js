53f7563bb526d86927b52a81ced62b4be659e57a
xuanhai0913 <xuanhai0913750452@gmail.com>
2025-11-27 09:13:51 +0700

    fix(logging): ensure all console logs are silenced in all environments

--- frontend/mobile/evChargingApp/index.js
+++ frontend/mobile/evChargingApp/index.js
@@ -6,20 +6,18 @@ import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-// Silence all React Native warnings and console logs for demo/release builds
+// Silence all React Native warnings and console logs in ALL environments (Dev + Release)
 LogBox.ignoreAllLogs(true);
-if (!__DEV__) {
-  const emptyFn = () => {};
-  // eslint-disable-next-line no-console
-  console.log = emptyFn;
-  // eslint-disable-next-line no-console
-  console.info = emptyFn;
-  // eslint-disable-next-line no-console
-  console.warn = emptyFn;
-  // eslint-disable-next-line no-console
-  console.error = emptyFn;
-  // eslint-disable-next-line no-console
-  console.debug = emptyFn;
-}
+const emptyFn = () => {};
+// eslint-disable-next-line no-console
+console.log = emptyFn;
+// eslint-disable-next-line no-console
+console.info = emptyFn;
+// eslint-disable-next-line no-console
+console.warn = emptyFn;
+// eslint-disable-next-line no-console
+console.error = emptyFn;
+// eslint-disable-next-line no-console
+console.debug = emptyFn;
 
 AppRegistry.registerComponent(appName, () => App);
